3f6764d0fd5b473028c02c6679951c93f6b76c8e
Yapeng <ya-peng.zhao@hp.com>
2021-05-17 13:21:44 +0800

    add new project.

---
+++ WPFEffects-master/Modules/AboutMe/AboutMeModuleView.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFEffects.Modules.AboutMe
+{
+    /// <summary>
+    /// AboutMeModuleView.xaml 的交互逻辑
+    /// </summary>
+    public partial class AboutMeModuleView : BaseModuleView
+    {
+        public AboutMeModuleView()
+        {
+            InitializeComponent();
+
+            this.MenuToMall.MouseLeftButtonDown += MenuToMall_MouseLeftButtonDown;
+
+            this.Loaded += AboutMeModuleView_Loaded;
+            this.Unloaded += AboutMeModuleView_Unloaded;
+        }
+
+        private void AboutMeModuleView_Unloaded(object sender, RoutedEventArgs e)
+        {
+            StopFrameAnim();
+        }
+
+        private void AboutMeModuleView_Loaded(object sender, RoutedEventArgs e)
+        {
+            PlayFrameAnim();
+        }
+
+        private void MenuToMall_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Process proc = new System.Diagnostics.Process();
+            proc.StartInfo.FileName = "https://shop173071246.taobao.com";
+            proc.Start();
+        }
+
+        private void PlayFrameAnim()
+        {
+            Storyboard anim = this.TryFindResource("Sb.BgAnim") as Storyboard;
+            if (anim != null)
+                anim.Begin();
+        }
+
+        private void StopFrameAnim()
+        {
+            Storyboard anim = this.TryFindResource("Sb.BgAnim") as Storyboard;
+            if (anim != null)
+                anim.Stop();
+        }
+    }
+}
---
+++ WPFEffects-master/README.md
@@ -0,0 +1,12 @@
+简版 WPF UI 框架
+
+![Alt text](https://github.com/DuelWithSelf/WPFEffects/blob/master/Screenshots/Effect1.JPG)
+
+Screenshots:
+
+![Alt text](https://github.com/DuelWithSelf/WPFEffects/blob/master/Screenshots/WPFEffect3.gif)
+![Alt text](https://github.com/DuelWithSelf/WPFEffects/blob/master/Screenshots/WPFEffect4.gif)
+![Alt text](https://github.com/DuelWithSelf/WPFEffects/blob/master/Screenshots/WPFEffect5.gif)
+![Alt text](https://github.com/DuelWithSelf/WPFEffects/blob/master/Screenshots/WPFEffect6.gif)
+![Alt text](https://github.com/DuelWithSelf/WPFEffects/blob/master/Screenshots/WPFEffect2.gif)
+
